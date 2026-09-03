86ff56d0f7cf76ca7908aaa7859a8b7280467a75
Priyanshu <priyanshumauryacontact@gmail.com>
2026-09-03 17:17:03 +0530

    Done with user_expense method

--- expense.py
+++ expense.py
@@ -6,5 +6,7 @@ class Expense:
         self.category = category
         self.amount = amount
     
-    
+    def __repr__(self):
+        
+        return f"<Expense: {self.name}, {self.category}, ${self.amount:.2f} >"
     
--- expense_tracker.py
+++ expense_tracker.py
@@ -1,6 +1,9 @@
+from expense import Expense
+
 def main():
     
-    user_expense()
+    expense = user_expense()
+    print(expense)
     
     save_expense_to_file()
     
@@ -8,7 +11,32 @@ def main():
 
 def user_expense():
     
-    pass
+    expense_name = input("Enter the name of expense: ")
+    expense_amount = float(input("Enter the expense amount: "))
+    
+    expense_category = ["🍕Food", "🏚️Home", "💼Work", "🎉Fun", "✨Miscellaneous"]
+
+    while True:
+        
+        print("Select a Category")
+
+        for i, categroy_name in enumerate(expense_category):
+            
+            print(f"{i + 1}. {categroy_name}")
+        
+        
+            
+        selected_index = int(input("Enter the category [1-5]: ")) - 1
+        
+        if selected_index in range(len(expense_category)):
+            
+            selected_category = expense_category[selected_index]
+            new_expense = Expense(name = expense_name, category = selected_category, amount = expense_amount)
+            return new_expense
+            
+        else:
+            print("Invalid Input")
+            
 
 def save_expense_to_file():
     
