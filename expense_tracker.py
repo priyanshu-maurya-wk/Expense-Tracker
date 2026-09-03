5392f75a43603e21bb5ac94bddc2d7e9d9e7daf4
Priyanshu <priyanshumauryacontact@gmail.com>
2026-09-03 16:41:01 +0530

    Created Main Structure of Project

---
+++ expense_tracker.py
@@ -0,0 +1,23 @@
+def main():
+    
+    user_expense()
+    
+    save_expense_to_file()
+    
+    summerize_expense()
+
+def user_expense():
+    
+    pass
+
+def save_expense_to_file():
+    
+    pass
+
+def summerize_expense():
+
+    pass
+
+if __name__ == "__main__":
+    
+    main()
